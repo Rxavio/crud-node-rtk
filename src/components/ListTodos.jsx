f03cd6546ba04db398cbadee196b9f53550695f6
xavioo <rucahoxavi@gmail.com>
2022-01-20 07:44:36 +0200

    display all todos list

--- src/components/ListTodos.jsx
+++ src/components/ListTodos.jsx
@@ -1,6 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getTodos } from "../features/todosSlice";
+import moment from "moment";
+import { CircularProgress, Card } from "@mui/material";
 import "../App.css";
 
 const ListTodos = ({ setTodo }) => {
@@ -16,7 +18,21 @@ const ListTodos = ({ setTodo }) => {
   return (
     <div>
      <h2> You have {todos && todos.length} tasks </h2>
-      <div>List of Tasks...</div>
+      {/* <div>List of Tasks...</div> */}
+      {todosState.getTodosStatus === "pending" ? <CircularProgress /> : null}
+      {todos.map((todo) => (
+        <Card
+          variant="outlined"
+          sx={{
+            padding: "0.7rem",
+            marginBottom: "2rem",
+          }}
+          key={todo._id}
+          >
+          <h3>{todo.task}</h3>
+          <p>Added: {moment(todo.date).fromNow()}</p>
+        </Card>
+      ))}
     </div>
   );
 };
